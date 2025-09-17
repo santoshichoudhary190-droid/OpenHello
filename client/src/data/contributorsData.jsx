108ed55e414f185546026edd13fe3cf43484f89c
santoshi chaudhary <santoshichoudhary190@gmail.com>
2025-09-17 13:09:36 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,7 +4,13 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Santoshi chaudhary",
+    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
+    githubUrl: "https://github.com/santoshichoudhary190-droid",
+  },
   
+
  
   // Add more contributors here
 ];
